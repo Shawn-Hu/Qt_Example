312ba17620fce4ea9fad4f3179c9ecc2531e500e
U-06WHDO51YVQC2JL\Administrator <Administrator@06WHDO51YVQC2JL.(none)>
2014-08-21 17:45:39 -0700

    fix the header error

--- build-Qt_Plot-Desktop_Qt_5_3_MinGW_32bit-Debug/debug/moc_delegate.cpp
+++ build-Qt_Plot-Desktop_Qt_5_3_MinGW_32bit-Debug/debug/moc_delegate.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../test/Qt_Example/Qt_Plot/delegate.h"
+#include "../../../../../te/Qt_Example/Qt_Plot/delegate.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
