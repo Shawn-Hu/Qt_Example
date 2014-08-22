9cbd700fec8ac4a8658884f31c150883a9ca43a8
U-06WHDO51YVQC2JL\Administrator <Administrator@06WHDO51YVQC2JL.(none)>
2014-08-21 17:16:20 -0700

    Add delegate.h

---
+++ Qt_Plot/delegate.cpp
@@ -0,0 +1,31 @@
+#include "delegate.h"
+
+Delegate::Delegate(QObject *parent) :
+    QItemDelegate (parent)
+{
+}
+
+
+QWidget *Delegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QLineEdit *edit=new QLineEdit(parent);
+    return edit;
+
+}
+void Delegate::setEditorData(QWidget *editor, const QModelIndex &index) const
+{
+   QString value = index.model()->data(index,Qt::EditRole).toString();
+   QLineEdit *lineditor = static_cast<QLineEdit*>(editor);
+   lineditor->setText(value);
+
+}
+void Delegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
+{
+    QLineEdit *lineditor = static_cast<QLineEdit*>(editor);
+    QString value = lineditor->text();
+    model->setData(index,value , Qt::EditRole);
+}
+void Delegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    editor->setGeometry(option.rect);
+}
