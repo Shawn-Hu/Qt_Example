b489f6c18b137cbd25a4cadd4b70b2ba5b8876fb
U-06WHDO51YVQC2JL\Administrator <Administrator@06WHDO51YVQC2JL.(none)>
2014-08-21 17:29:24 -0700

    update document

--- build-Qt_Plot-Desktop_Qt_5_3_MinGW_32bit-Debug/debug/moc_delegate.cpp
+++ build-Qt_Plot-Desktop_Qt_5_3_MinGW_32bit-Debug/debug/moc_delegate.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Qt_Plot/delegate.h"
+#include "../../../../test/Qt_Example/Qt_Plot/delegate.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- build-Qt_Plot-Desktop_Qt_5_3_MinGW_32bit-Debug/debug/moc_qcustomplot.cpp
+++ build-Qt_Plot-Desktop_Qt_5_3_MinGW_32bit-Debug/debug/moc_qcustomplot.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Qt_Plot/qcustomplot.h"
+#include "../../../../test/Qt_Example/Qt_Plot/qcustomplot.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #include <QtCore/QList>
